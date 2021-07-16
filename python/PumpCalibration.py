df749dfd9a97060f6379cd59e368618a8f7c740c
miraclezero <flmajia@gmail.com>
2021-07-16 11:58:33 -0500

    update instruction for calibration

--- python/PumpCalibration.py
+++ python/PumpCalibration.py
@@ -51,7 +51,7 @@ def pump_calibration(step_size, fname):
                     mover.move("forward", step)
 
             act_count += 1
-
+            print("count" + act_count)
             if act_count == 15:
                 response = input("please enter the amount measured (numeric value) or scan the command id again to exit: ").strip()
                 # if the same command RFID is scanned again, exit the while loop
@@ -72,6 +72,7 @@ def pump_calibration(step_size, fname):
                     step = math.ceil( (step / (avg_measured_value / 5)) *60  )
                     
                     print("modified step: {}".format(step))
+                    print("Touch spout to measure again")
                 
                 act_count = 0
 
