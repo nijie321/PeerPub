707498ad00dadf2ed495b1fe748edb47b018d4d8
miraclezero <flmajia@gmail.com>
2022-06-07 11:30:54 -0500

    save images in folder

--- utility_script/photo.sh
+++ utility_script/photo.sh
@@ -1,7 +1,12 @@
 # 1920 (H) x 1080 (V) ,1280 (H) x 1024 (V),1280 (H) x 720 (V)
 #NAME="/home/pi/image/"`cat /home/pi/deviceid``date`".jpg"
 #echo $NAME
-BoxID=$( cat /home/pi/deviceid )
-echo $BoxID
+#BoxID=$( cat /home/pi/deviceid )
+#echo $BoxID
+echo $1
+echo $2
+echo $3
+mkdir "/home/pi/image/$(date +"%Y-%m-%d_$3_$2")"
 
-fswebcam -r 1920x1080 -S 19 --set brightness=100% --set contrast=0% --loop 1  --no-banner /home/pi/image/"%F_%T_$BoxID".jpg
+timeout $1 fswebcam -r 1920x1080 -S 19 --set brightness=100% --set contrast=0% --loop 1  --no-banner /home/pi/image/$(date +"%Y-%m-%d_$3_$2")/"%F_%T_$3_$2".jpg
+
